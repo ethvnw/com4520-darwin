9f1e04d107b9f60d88c05dcd739172da0833ecda
Lexi Wheadon <cdwheadon1@sheffield.ac.uk>
2025-02-26 16:29:18 +0000

    began testing plan

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+### tests to do ###
+## generator ##
+# ensure connectivity #
+# ensure minimalism # 
+
+## mutator ##
+# add state #
+# remove state # 
+# change trigger output # 
+# change transition destination # 
