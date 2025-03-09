43a777dbad06a621207e3b405460a3b115b8da58
Lexi Wheadon <cdwheadon1@sheffield.ac.uk>
2025-03-09 22:05:09 +0000

    setup mutator testing

--- fsm_gen/tests/test_mutator.py
+++ fsm_gen/tests/test_mutator.py
@@ -1,3 +1,18 @@
+import pytest
+import collections
+import random
+import pickle
+import os
+
+from fsm_gen.generator import FSMGenerator
+from fsm_gen.mutator import Mutator
+
+fsm = FSMGenerator(num_states=5, num_inputs=3)
+fsm.draw("original.png")
+
+mutated_fsm = Mutator(fsm).create_mutated_fsm()
+mutated_fsm.draw("mutated.png")
+
 ## mutator ##
 # add state #
 # remove state # 
@@ -27,17 +42,3 @@
     # dfs()
 
 # get_machine_properties()
-
-
-
-
-
-
-# from fsm_gen.generator import FSMGenerator
-# from fsm_gen.mutator import Mutator
-
-# fsm = FSMGenerator(num_states=5, num_inputs=3)
-# fsm.draw("original.png")
-
-# mutated_fsm = Mutator(fsm).create_mutated_fsm()
-# mutated_fsm.draw("mutated.png")
