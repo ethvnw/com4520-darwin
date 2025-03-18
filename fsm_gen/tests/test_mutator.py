c2021f7fb3378e1facae6cacf9e614c74913f97b
Lexi Wheadon <cdwheadon1@sheffield.ac.uk>
2025-03-18 20:18:42 +0000

    refactored mutator and mutated_fsm tests to not use private methods

--- fsm_gen/tests/test_mutator.py
+++ fsm_gen/tests/test_mutator.py
@@ -8,19 +8,43 @@ from fsm_gen.machine import Machine
 from fsm_gen.mutator import Mutator
 
 @pytest.fixture
-def sample_fsm():
+def testing_fsm(): # generate fsm of specified states and inputs to mutate from
     fsm = FSMGenerator(num_states=5, num_inputs=3)
     fsm.draw("original.png")
     return fsm
 
 @pytest.fixture
-def mutator(sample_fsm):
-    return Mutator(sample_fsm)
+def mutator(testing_fsm): # create an object of the mutator class
+    return Mutator(testing_fsm)
 
 @pytest.fixture
-def mutated_fsm(mutator):
-    mutator._mutate()
+def mutated_fsm(mutator): # mutate the testing fsm
+    mutator.create_mutated_fsm()
     return mutator.fsm
+
+def test_mutation_effects(): # had 1 error in 1000 test runs
+    """Test if all types of mutations are applied by running multiple mutations on fresh FSM instances."""
+    mutation_types_encountered = set()
+    expected_mutations = {"add_state", "remove_state", "add_transition", "remove_transition", "modify_transition"} ### STILL TO FIX
+    for _ in range(20):  # Run multiple mutations to cover all types
+        fsm = FSMGenerator(num_states=5, num_inputs=3)  # Create a fresh FSM
+        mutator = Mutator(fsm)  # Create a new Mutator instance
+        initial_state_count = len(fsm.states)
+        initial_transition_count = len(fsm.transitions)
+        original_transitions = pickle.dumps(fsm.transitions)
+        mutator.create_mutated_fsm()
+        if len(mutator.fsm.states) > initial_state_count:
+            mutation_types_encountered.add("add_state")
+        elif len(mutator.fsm.states) < initial_state_count:
+            mutation_types_encountered.add("remove_state")
+        if len(mutator.fsm.transitions) > initial_transition_count:
+            mutation_types_encountered.add("add_transition")
+        elif len(mutator.fsm.transitions) < initial_transition_count:
+            mutation_types_encountered.add("remove_transition")
+        if pickle.dumps(mutator.fsm.transitions) != original_transitions:
+            mutation_types_encountered.add("modify_transition")
+            
+    assert mutation_types_encountered == expected_mutations, f"Missing mutation types: {expected_mutations - mutation_types_encountered}"
 
 def test_create_mutated_fsm(mutated_fsm): #POSSIBLE TODO 
     mutated_fsm.draw("mutated.png")
@@ -29,8 +53,8 @@ def test_create_mutated_fsm(mutated_fsm): #POSSIBLE TODO
     assert isinstance(mutated_fsm, FSMGenerator)
     assert os.path.exists("mutated.pkl")
 
-def test_mutation_application(mutator, sample_fsm): # REUNDERSTAND AND TODO # errored roughly once in 100
-    original_fsm = pickle.dumps(sample_fsm)
+def test_mutation_application(mutator, testing_fsm): #ensures a previously applied mutation is not then mutated back to the original fsm
+    original_fsm = pickle.dumps(testing_fsm)
     mutator._mutate()
     assert mutator.mutations_applied
     assert pickle.dumps(mutator.fsm) != original_fsm
@@ -46,31 +70,6 @@ def test_mutated_fsm_determinism(mutator): #TODO
     assert True
 
 '''END OF BAD TESTS'''
-
-def test_mutation_effects(): # had 1 error in 1000 test runs
-    """Test if all types of mutations are applied by running multiple mutations on fresh FSM instances."""
-    mutation_types_encountered = set()
-    expected_mutations = {"add_state", "remove_state", "add_transition", "remove_transition", "modify_transition"} ### STILL TO FIX
-    for _ in range(20):  # Run multiple mutations to cover all types
-        fsm = FSMGenerator(num_states=5, num_inputs=3)  # Create a fresh FSM
-        mutator = Mutator(fsm)  # Create a new Mutator instance
-        initial_state_count = len(fsm.states)
-        initial_transition_count = len(fsm.transitions)
-        original_transitions = pickle.dumps(fsm.transitions)
-        mutator._mutate()
-        if len(mutator.fsm.states) > initial_state_count:
-            mutation_types_encountered.add("add_state")
-        elif len(mutator.fsm.states) < initial_state_count:
-            mutation_types_encountered.add("remove_state")
-        if len(mutator.fsm.transitions) > initial_transition_count:
-            mutation_types_encountered.add("add_transition")
-        elif len(mutator.fsm.transitions) < initial_transition_count:
-            mutation_types_encountered.add("remove_transition")
-        if pickle.dumps(mutator.fsm.transitions) != original_transitions:
-            mutation_types_encountered.add("modify_transition")
-            
-    assert mutation_types_encountered == expected_mutations, f"Missing mutation types: {expected_mutations - mutation_types_encountered}"
-
 
 @pytest.fixture(scope="function", autouse=True)
 def cleanup():
