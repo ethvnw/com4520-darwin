7d263deb6fc207f0efece5e8dd36231d0eb63a30
Lexi Wheadon <cdwheadon1@sheffield.ac.uk>
2025-03-19 23:51:31 +0000

    changed how checking mutations was done to prevent excessive loops for new tests

--- fsm_gen/tests/test_mutator.py
+++ fsm_gen/tests/test_mutator.py
@@ -3,6 +3,7 @@ import collections
 import random
 import pickle
 import os
+import re
 from fsm_gen.generator import FSMGenerator
 from fsm_gen.machine import Machine
 from fsm_gen.mutator import Mutator
@@ -22,16 +23,79 @@ def mutated_fsm(mutator): # mutate the testing fsm
     mutator.create_mutated_fsm()
     return mutator.fsm
 
+def test_add_state():
+    fsm = FSMGenerator(num_states=5, num_inputs=3)
+    mutator = Mutator(fsm)
+    initial_state_count = len(fsm.states)
+    mutator._add_state()
+    assert len(mutator.fsm.states) > initial_state_count
+
+def test_remove_state():
+    fsm = FSMGenerator(num_states=5, num_inputs=3)
+    mutator = Mutator(fsm)
+    initial_state_count = len(fsm.states)
+    mutator._remove_state()
+    assert len(mutator.fsm.states) < initial_state_count
+    
+def test_change_trigger_output():
+    fsm = FSMGenerator(num_states=5, num_inputs=3)
+    mutator = Mutator(fsm)
+    initial_trigger = []
+    initial_source = []
+    initial_dest = []
+    found = False
+    for i in range(0,len(fsm.transitions)):
+        initial_trigger.append(fsm.transitions[i]["trigger"])
+        initial_source.append(fsm.transitions[i]["source"])
+        initial_dest.append(fsm.transitions[i]["dest"])
+    print(fsm.transitions)
+    mutator._change_trigger_output()
+    print(fsm.transitions)
+    for i in range(0,len(fsm.transitions)):
+        for j in range(0,len(fsm.transitions)):
+            if fsm.transitions[i]["dest"] == initial_dest[j] and fsm.transitions[i]["source"] == initial_source[j]:
+                if fsm.transitions[i]["trigger"] != initial_trigger[j]:
+                    found = True
+                    break
+        if found == True:
+            break
+    assert found
+    
+def test_change_trans_dest():
+    fsm = FSMGenerator(num_states=5, num_inputs=3)
+    mutator = Mutator(fsm)
+    initial_trigger = []
+    initial_source = []
+    initial_dest = []
+    found = False
+    for i in range(0,len(fsm.transitions)):
+        initial_trigger.append(fsm.transitions[i]["trigger"])
+        initial_source.append(fsm.transitions[i]["source"])
+        initial_dest.append(fsm.transitions[i]["dest"])
+    print(fsm.transitions)
+    mutator._change_trans_dest()
+    print(fsm.transitions)
+    for i in range(0,len(fsm.transitions)):
+        for j in range(0,len(fsm.transitions)):
+            if fsm.transitions[i]["trigger"] == initial_trigger[j] and fsm.transitions[i]["source"] == initial_source[j]:
+                if fsm.transitions[i]["dest"] != initial_dest[j]:
+                    found = True
+                    break
+        if found == True:
+            break
+    assert found
+'''
 def test_mutation_effects(): # had 1 error in 1000 test runs
     """Test if all types of mutations are applied by running multiple mutations on fresh FSM instances."""
     mutation_types_encountered = set()
-    expected_mutations = {"add_state", "remove_state", "add_transition", "remove_transition", "modify_transition"} ### STILL TO FIX
+    expected_mutations = {"add_state", "remove_state", "change_trigger_output", "change_transition_destination"} ### STILL TO FIX
     for _ in range(20):  # Run multiple mutations to cover all types
         fsm = FSMGenerator(num_states=5, num_inputs=3)  # Create a fresh FSM
         mutator = Mutator(fsm)  # Create a new Mutator instance
         initial_state_count = len(fsm.states)
         initial_transition_count = len(fsm.transitions)
         original_transitions = pickle.dumps(fsm.transitions)
+        
         mutator.create_mutated_fsm()
         if len(mutator.fsm.states) > initial_state_count:
             mutation_types_encountered.add("add_state")
@@ -45,7 +109,7 @@ def test_mutation_effects(): # had 1 error in 1000 test runs
             mutation_types_encountered.add("modify_transition")
             
     assert mutation_types_encountered == expected_mutations, f"Missing mutation types: {expected_mutations - mutation_types_encountered}"
-
+'''
 def test_create_mutated_fsm(mutated_fsm): #POSSIBLE TODO 
     mutated_fsm.draw("mutated.png")
     with open("mutated.pkl", "wb") as f:
@@ -68,8 +132,16 @@ def test_mutation_preserves_connectivity(mutator): #TODO
 def test_mutated_fsm_determinism(mutator): #TODO
 
     assert True
-
+    
 '''END OF BAD TESTS'''
+
+''' Confused by how to test this'''
+'''
+def test_get_machine_properties(testing_fsm):
+    fsm = FSMGenerator(num_states=5, num_inputs=3)
+    fsm._check_connectivity() = True
+    fsm._check_determinism() = True
+    assert Mutator.get_machine_properties(fsm) == (f"\nConnected: True, Deterministic: True")'''
 
 @pytest.fixture(scope="function", autouse=True)
 def cleanup():
@@ -86,7 +158,7 @@ def cleanup():
 
 ## all mutator functions ##
 
-# create_mutated_fsm() # done ?
+# create_mutated_fsm() # done
 
 # _mutate() # done
 
@@ -96,14 +168,12 @@ def cleanup():
 
 # _change_trigger_output() # done ? TODO
 
-# _get_num_transitions_exclude_loops() ? TODO
-
 # _change_trans_dest() # done ? TODO
 
-# _check determinism() # tested elsewhere TODO
+# _get_num_transitions_exclude_loops() #  TODO
 
-# _check connectivity() # tested elsewhere TODO
+# _check determinism() #  TODO
 
-    # dfs() # tested elsewhere
+# _check connectivity() # TODO
 
-# get_machine_properties() # only prints previous 2 function outputs
+# get_machine_properties() # TODO
